72a10b04fec99cecd0b9ed4c385c674720b7440d
wachirarobbinson <ngirigacharobbinson@gmail.com>
2023-08-23 12:02:11 +0300

    completed assignment

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,21 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+    def walk(self):
+        print("The person is walking.")
+
+
+guido = Person()
+guido = Person()
+
+
+
+guido.talk()
+guido.walk()      
+
+
+
+   
+ 
